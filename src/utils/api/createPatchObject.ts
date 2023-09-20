21f7d598282d432ec9365893e985a9cb34ffb88d
daniel-slaugh <daniel.slaugh@hotmail.com>
2023-09-20 11:29:39 -0600

    Add unit testing on push/pull request

--- src/utils/api/createPatchObject.ts
+++ src/utils/api/createPatchObject.ts
@@ -17,13 +17,16 @@
 export function createPatchObject(original: any, updated: any): any {
   const differences: any = {}
   for (let key in updated) {
-    if (
+    if (Array.isArray(updated[key])) {
+      if (JSON.stringify(original[key]) !== JSON.stringify(updated[key])) {
+        differences[key] = updated[key]
+      }
+    } else if (
       original[key] &&
       typeof original[key] === 'object' &&
       updated[key] &&
       typeof updated[key] === 'object'
     ) {
-      // If the value is an object, we recurse
       const nestedDiff = createPatchObject(original[key], updated[key])
       if (Object.keys(nestedDiff).length > 0) {
         differences[key] = nestedDiff
