6a28f716d215a9b00924c8e4394a0d6df2541bac
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2023-10-13 18:09:57 -0600

    Troubleshooting OAuth

--- src/store/authentication.ts
+++ src/store/authentication.ts
@@ -164,10 +164,6 @@ export const useAuthStore = defineStore({
     },
     async OAuthLogin(provider: OAuthProvider, callback?: () => any) {
       const handleMessage = async (event: MessageEvent) => {
-        console.log('!!!!!!!!!!!!!!!')
-        console.log(event.origin)
-        console.log(APP_URL)
-        console.log(event.data)
         if (
           event.origin !== APP_URL ||
           !event.data.hasOwnProperty('accessToken')
