120d3ea647c0830f46fd3ebaf900522d2182db24
daniel-slaugh <daniel.slaugh@hotmail.com>
2023-11-15 11:20:55 -0700

    Port datastreamStore to composition API

--- src/store/datastreams.ts
+++ src/store/datastreams.ts
@@ -1,161 +1,144 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 import { Datastream } from '@/types'
 import { api } from '@/services/apiMethods'
 import { ENDPOINTS } from '@/constants'
 
-export const useDatastreamStore = defineStore('datastreams', {
-  state: () => ({
-    datastreams: {} as Record<string, Datastream[]>,
-    loaded: false,
-    fetching: false,
-    loadedUsersDatastreams: false,
-  }),
-  getters: {
-    getDatastreamsByParameter:
-      (state) =>
-      (parameter_name: keyof Datastream, parameter_id: string | number) => {
-        const datastreams = Object.values(state.datastreams).flat()
-        return datastreams.filter((ds) => ds[parameter_name] === parameter_id)
-      },
-  },
-  actions: {
-    async fetchDatastreams(reload = false) {
-      // if (this.datastreams && !reload) return
-      try {
-        const data = await api.fetch(ENDPOINTS.DATASTREAMS)
-        let newDatastreams = this.groupDatastreamsByThingId(data)
-        this.$patch({ datastreams: newDatastreams })
-        this.loaded = true
-      } catch (error) {
-        console.error('Error fetching datastreams from DB', error)
-      }
-    },
-    async fetchUsersDatastreams(reload = false) {
-      try {
-        // if (this.fetching || (this.loadedUsersDatastreams && !reload)) return
-        if (this.fetching) return
-        this.fetching = true
-        const data = await api.fetch(ENDPOINTS.DATASTREAMS.FOR_USER)
-        let datastreamMap = this.groupDatastreamsByThingId(data)
+export const useDatastreamStore = defineStore('datastreams', () => {
+  const datastreams = ref<Record<string, Datastream[]>>({})
+  const loaded = ref(false)
+  const fetching = ref(false)
+  const loadedUsersDatastreams = ref(false)
 
-        // Replace the existing datastreams with the new set for each thingId
-        this.$patch((state) => {
-          Object.keys(datastreamMap).forEach((thingId) => {
-            state.datastreams[thingId] = datastreamMap[thingId]
-          })
-        })
+  const getDatastreamsByParameter = (name: keyof Datastream, id: string) =>
+    Object.values(datastreams.value)
+      .flat()
+      .filter((ds) => ds[name] === id)
 
-        this.loadedUsersDatastreams = true
-      } catch (error) {
-        console.error('Error fetching datastreams from DB', error)
-      } finally {
-        this.fetching = false
-      }
-    },
-    groupDatastreamsByThingId(datastreams: Datastream[]) {
-      const grouped: Record<string, Datastream[]> = {}
-      datastreams.forEach((datastream: Datastream) => {
-        if (!grouped[datastream.thingId]) {
-          grouped[datastream.thingId] = []
-        }
-        grouped[datastream.thingId].push(datastream)
-      })
-      return grouped
-    },
-    async fetchDatastreamsByThingId(id: string, reload = false) {
-      if (this.fetching) return
-      this.fetching = true
-      try {
-        const data = await api.fetch(ENDPOINTS.DATASTREAMS.FOR_THING(id))
-        this.datastreams[id] = data
-        return data
-      } catch (error) {
-        console.error(`Error fetching datastreams by thingID`, error)
-      } finally {
-        this.fetching = false
-      }
-    },
-    async updateDatastream(datastream: Datastream) {
-      try {
-        const data = await api.patch(
-          `${ENDPOINTS.DATASTREAMS}/${datastream.id}`,
-          datastream,
-          this.getDatastreamById(datastream.id)
-        )
-        const datastreamsForThing = this.datastreams[data.thingId]
-        const index = datastreamsForThing.findIndex((ds) => ds.id === data.id)
-        if (index !== -1 && data) datastreamsForThing[index] = data
-      } catch (error) {
-        console.error('Error updating datastream', error)
-      }
-    },
-    async createDatastream(newDatastream: Datastream) {
-      try {
-        const data = await api.post(ENDPOINTS.DATASTREAMS, newDatastream)
-        if (!this.datastreams[newDatastream.thingId]) {
-          this.datastreams[newDatastream.thingId] = []
-        }
-        this.datastreams[newDatastream.thingId].push(data)
-      } catch (error) {
-        console.error('Error creating datastream', error)
-      }
-    },
-    async deleteDatastream(id: string, thingId: string) {
-      try {
-        await api.delete(`${ENDPOINTS.DATASTREAMS}/${id}`)
+  const fetchDatastreams = async (reload = false) => {
+    // if (this.datastreams && !reload) return
+    try {
+      const data = await api.fetch(ENDPOINTS.DATASTREAMS)
+      let newDatastreams = groupDatastreamsByThingId(data)
+      datastreams.value = newDatastreams
+      loaded.value = true
+    } catch (error) {
+      console.error('Error fetching datastreams from DB', error)
+    }
+  }
 
-        const datastreams = this.datastreams[thingId].filter(
-          (datastream) => datastream.id !== id
-        )
-        this.$patch({
-          datastreams: { ...this.datastreams, [thingId]: datastreams },
-        })
-      } catch (error) {
-        console.error(`Error deleting datastream with id ${id}`, error)
-      }
-    },
-    getDatastreamForThingById(
-      thingId: string,
-      datastreamId: string
-    ): Datastream | null {
-      const thingDatastreams = this.datastreams[thingId]
-      if (thingDatastreams) {
-        const datastream = thingDatastreams.find((ds) => ds.id === datastreamId)
-        return datastream ? datastream : null
-      }
-      return null
-    },
-    getDatastreamById(datastreamId: string): Datastream | null {
-      for (const thingId in this.datastreams) {
-        const thingDatastreams = this.datastreams[thingId]
-        const datastream = thingDatastreams.find((ds) => ds.id === datastreamId)
-        if (datastream) {
-          return datastream
-        }
-      }
-      return null
-    },
-    // TODO: This shouldn't be in the store
-    async downloadDatastream(id: string) {
-      try {
-        const data = await api.fetch(ENDPOINTS.DATASTREAMS.CSV(id))
-        // Create a Blob from the received data
-        const blob = new Blob([data], { type: 'text/csv' })
+  const fetchUsersDatastreams = async (reload = false) => {
+    // if (this.fetching || (this.loadedUsersDatastreams && !reload)) return
+    try {
+      fetching.value = true
+      const data = await api.fetch(ENDPOINTS.DATASTREAMS.FOR_USER)
+      let datastreamMap = groupDatastreamsByThingId(data)
 
-        // Create a download link element
-        const link = document.createElement('a')
-        link.href = window.URL.createObjectURL(blob)
-        link.download = `datastream_${id}.csv`
-
-        // Append the link to the document and trigger a click event
-        document.body.appendChild(link)
-        link.click()
-
-        // Clean up by removing the link
-        document.body.removeChild(link)
-      } catch (error) {
-        console.error('Error downloading datastream CSV', error)
+      // Replace the existing datastreams with the new set for each thingId
+      for (const thingId in datastreamMap) {
+        datastreams.value[thingId] = datastreamMap[thingId]
       }
-    },
-  },
+
+      loadedUsersDatastreams.value = true
+    } catch (error) {
+      console.error('Error fetching user datastreams from DB', error)
+    } finally {
+      fetching.value = false
+    }
+  }
+
+  const groupDatastreamsByThingId = (datastreamsArray: Datastream[]) => {
+    const grouped: Record<string, Datastream[]> = {}
+    datastreamsArray.forEach((datastream) => {
+      if (!grouped[datastream.thingId]) {
+        grouped[datastream.thingId] = []
+      }
+      grouped[datastream.thingId].push(datastream)
+    })
+    return grouped
+  }
+
+  const fetchDatastreamsByThingId = async (id: string, reload = false) => {
+    if (fetching.value) return
+    fetching.value = true
+    try {
+      const data = await api.fetch(ENDPOINTS.DATASTREAMS.FOR_THING(id))
+      datastreams.value[id] = data
+      return data
+    } catch (error) {
+      console.error(`Error fetching datastreams by thingID`, error)
+    } finally {
+      fetching.value = false
+    }
+  }
+  const updateDatastream = async (datastream: Datastream) => {
+    try {
+      const data = await api.patch(
+        `${ENDPOINTS.DATASTREAMS}/${datastream.id}`,
+        datastream
+      )
+      const datastreamsForThing = datastreams.value[data.thingId]
+      const index = datastreamsForThing.findIndex((ds) => ds.id === data.id)
+      if (index !== -1 && data) {
+        datastreamsForThing[index] = data
+      }
+    } catch (error) {
+      console.error('Error updating datastream', error)
+    }
+  }
+
+  const createDatastream = async (newDatastream: Datastream) => {
+    try {
+      const data = await api.post(ENDPOINTS.DATASTREAMS, newDatastream)
+      if (!datastreams.value[newDatastream.thingId]) {
+        datastreams.value[newDatastream.thingId] = []
+      }
+      datastreams.value[newDatastream.thingId].push(data)
+    } catch (error) {
+      console.error('Error creating datastream', error)
+    }
+  }
+
+  const deleteDatastream = async (id: string, thingId: string) => {
+    try {
+      await api.delete(`${ENDPOINTS.DATASTREAMS}/${id}`)
+      const updatedDatastreams = datastreams.value[thingId].filter(
+        (datastream) => datastream.id !== id
+      )
+      datastreams.value[thingId] = updatedDatastreams
+    } catch (error) {
+      console.error(`Error deleting datastream with id ${id}`, error)
+    }
+  }
+
+  const downloadDatastream = async (id: string) => {
+    try {
+      const data = await api.fetch(ENDPOINTS.DATASTREAMS.CSV(id))
+      const blob = new Blob([data], { type: 'text/csv' })
+      const link = document.createElement('a')
+      link.href = window.URL.createObjectURL(blob)
+      link.download = `datastream_${id}.csv`
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    } catch (error) {
+      console.error('Error downloading datastream CSV', error)
+    }
+  }
+
+  return {
+    datastreams,
+    loaded,
+    fetching,
+    loadedUsersDatastreams,
+    getDatastreamsByParameter,
+    fetchDatastreams,
+    fetchUsersDatastreams,
+    groupDatastreamsByThingId,
+    fetchDatastreamsByThingId,
+    updateDatastream,
+    createDatastream,
+    deleteDatastream,
+    downloadDatastream,
+  }
 })
