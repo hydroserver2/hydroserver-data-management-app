e137dd10bcf3b1b5a8f27b1ad3acf1c75c05e9bd
daniel-slaugh <daniel.slaugh@hotmail.com>
2023-11-16 09:24:56 -0700

    RQ to composition API

--- src/store/resultQualifiers.ts
+++ src/store/resultQualifiers.ts
@@ -1,79 +1,88 @@
 import { defineStore } from 'pinia'
-import { ResultQualifier } from '@/types'
+import { ref, computed } from 'vue'
+import type { ResultQualifier } from '@/types'
 import { api } from '@/services/apiMethods'
 import { ENDPOINTS } from '@/constants'
 
-export const useResultQualifierStore = defineStore('resultQualifiers', {
-  state: () => ({ resultQualifiers: [] as ResultQualifier[], loaded: false }),
-  getters: {
-    ownedResultQualifiers(): ResultQualifier[] {
-      return this.resultQualifiers.filter((rq) => rq.owner != null)
-    },
-    unownedResultQualifiers(): ResultQualifier[] {
-      return this.resultQualifiers.filter((rq) => rq.owner == null)
-    },
-  },
-  actions: {
-    sortResultQualifiers() {
-      this.resultQualifiers.sort((a, b) => a.code.localeCompare(b.code))
-    },
-    async fetchResultQualifiers() {
-      // if (this.loaded) return
-      try {
-        this.resultQualifiers = await api.fetch(ENDPOINTS.RESULT_QUALIFIERS)
-        this.sortResultQualifiers()
-        this.loaded = true
-      } catch (error) {
-        console.error('Error fetching result qualifiers from DB', error)
-      }
-    },
-    async updateResultQualifier(resultQualifier: ResultQualifier) {
-      try {
-        await api.patch(
-          ENDPOINTS.RESULT_QUALIFIERS.ID(resultQualifier.id),
-          resultQualifier,
-          this.getById(resultQualifier.id)
-        )
-        const index = this.resultQualifiers.findIndex(
-          (rq) => rq.id === resultQualifier.id
-        )
-        if (index !== -1) this.resultQualifiers[index] = resultQualifier
-        this.sortResultQualifiers()
-      } catch (error) {
-        console.error(`Error updating result qualifier with id`, error)
-      }
-    },
-    async createResultQualifier(resultQualifier: ResultQualifier) {
-      try {
-        const data = await api.post(
-          ENDPOINTS.RESULT_QUALIFIERS,
-          resultQualifier
-        )
-        this.resultQualifiers.push(data)
-        this.sortResultQualifiers()
-        return data
-      } catch (error) {
-        console.error('Error creating result qualifier', error)
-      }
-    },
-    async deleteResultQualifier(id: string) {
-      try {
-        await api.delete(ENDPOINTS.RESULT_QUALIFIERS.ID(id))
-        this.resultQualifiers = this.resultQualifiers.filter(
-          (rq) => rq.id !== id
-        )
-        this.sortResultQualifiers()
-      } catch (error) {
-        console.error('Error deleting result qualifier', error)
-      }
-    },
-    getById(id: string) {
-      const resultQualifier = this.resultQualifiers.find(
-        (rq) => rq.id.toString() === id.toString()
+export const useResultQualifierStore = defineStore('resultQualifiers', () => {
+  const resultQualifiers = ref<ResultQualifier[]>([])
+  const loaded = ref(false)
+
+  const ownedResultQualifiers = computed(() =>
+    resultQualifiers.value.filter((rq) => rq.owner != null)
+  )
+
+  const sortResultQualifiers = () => {
+    resultQualifiers.value.sort((a, b) => a.code.localeCompare(b.code))
+  }
+
+  const fetchResultQualifiers = async () => {
+    try {
+      resultQualifiers.value = await api.fetch(ENDPOINTS.RESULT_QUALIFIERS)
+      sortResultQualifiers()
+      loaded.value = true
+    } catch (error) {
+      console.error('Error fetching result qualifiers from DB', error)
+    }
+  }
+
+  const updateResultQualifier = async (resultQualifier: ResultQualifier) => {
+    try {
+      await api.patch(
+        ENDPOINTS.RESULT_QUALIFIERS.ID(resultQualifier.id),
+        resultQualifier
       )
-      if (!resultQualifier)
-        throw new Error(`Result Qualifier with id ${id} not found`)
-      return resultQualifier
-    },
-  },
+      const index = resultQualifiers.value.findIndex(
+        (rq) => rq.id === resultQualifier.id
+      )
+      if (index !== -1) resultQualifiers.value[index] = resultQualifier
+      sortResultQualifiers()
+    } catch (error) {
+      console.error(`Error updating result qualifier with id`, error)
+    }
+  }
+
+  const createResultQualifier = async (resultQualifier: ResultQualifier) => {
+    try {
+      const data = await api.post(ENDPOINTS.RESULT_QUALIFIERS, resultQualifier)
+      resultQualifiers.value.push(data)
+      sortResultQualifiers()
+      return data
+    } catch (error) {
+      console.error('Error creating result qualifier', error)
+    }
+  }
+
+  const deleteResultQualifier = async (id: string) => {
+    try {
+      await api.delete(ENDPOINTS.RESULT_QUALIFIERS.ID(id))
+      resultQualifiers.value = resultQualifiers.value.filter(
+        (rq) => rq.id !== id
+      )
+      sortResultQualifiers()
+    } catch (error) {
+      console.error('Error deleting result qualifier', error)
+    }
+  }
+
+  const getById = (id: string) => {
+    const resultQualifier = resultQualifiers.value.find(
+      (rq) => rq.id.toString() === id.toString()
+    )
+    if (!resultQualifier)
+      throw new Error(`Result Qualifier with id ${id} not found`)
+    return resultQualifier
+  }
+
+  return {
+    resultQualifiers,
+    loaded,
+    ownedResultQualifiers,
+    sortResultQualifiers,
+    fetchResultQualifiers,
+    updateResultQualifier,
+    createResultQualifier,
+    deleteResultQualifier,
+    getById,
+  }
 })
