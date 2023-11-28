44db156dd4a9dc570ec115d542efc5ea9dfc7598
daniel-slaugh <daniel.slaugh@hotmail.com>
2023-11-28 16:17:06 -0700

    Remove ownedThings from thing store

--- src/store/things.ts
+++ src/store/things.ts
@@ -1,16 +1,11 @@
 import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { Thing } from '@/types'
 import { api } from '@/services/api'
 
 export const useThingStore = defineStore('things', () => {
   const things = ref<Record<string, Thing>>({})
-  const loaded = ref(false)
   const fetching = ref(false)
-
-  const ownedThings = computed(() =>
-    Object.values(things.value).filter((thing) => thing.ownsThing)
-  )
 
   const fetchThings = async () => {
     try {
@@ -18,7 +13,6 @@ export const useThingStore = defineStore('things', () => {
       things.value = Object.fromEntries(
         data.map((thing: Thing) => [thing.id, thing])
       )
-      loaded.value = true
     } catch (error) {
       console.error('Error fetching things', error)
     }
@@ -39,9 +33,7 @@ export const useThingStore = defineStore('things', () => {
 
   return {
     things,
-    loaded,
     fetching,
-    ownedThings,
     fetchThings,
     fetchThingById,
   }
