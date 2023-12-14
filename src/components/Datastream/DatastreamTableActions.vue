43bc09a4e377561dc193a5b4bcc8b79d5fbb995a
daniel-slaugh <daniel.slaugh@hotmail.com>
2023-12-14 11:00:49 -0700

    Remove DataSourceLinkStore; Add Delete btn

--- src/components/Datastream/DatastreamTableActions.vue
+++ src/components/Datastream/DatastreamTableActions.vue
@@ -58,13 +58,7 @@
           <v-list-item
             prepend-icon="mdi-link-variant"
             title="Link Data Source"
-            @click="
-              handleLinkDataSource(
-                datastream.id,
-                datastream.data_source_id,
-                datastream.column
-              )
-            "
+            @click="openLinker = true"
           />
           <v-list-item
             prepend-icon="mdi-pencil"
@@ -77,7 +71,7 @@
           <v-list-item
             prepend-icon="mdi-delete"
             title="Delete Datastream"
-            @click="openDeleteModal(datastream)"
+            @click="openDelete = true"
           />
         </div>
         <v-list-item
@@ -96,7 +90,7 @@
     </v-menu>
   </v-row>
 
-  <v-dialog v-if="selectedDatastream" v-model="isDeleteModalOpen" width="40rem">
+  <v-dialog v-model="openDelete" width="40rem">
     <v-card>
       <v-card-title>
         <span class="text-h5">Confirm Deletion</span>
@@ -104,45 +98,40 @@
       <v-card-text>
         Are you sure you want to permanently delete this datastream and all the
         observations associated with it?
-        <br />
-        <br />
-        <strong>ID:</strong> {{ selectedDatastream.id }} <br />
       </v-card-text>
+      <v-card-text> <strong>ID:</strong> {{ datastream.id }} </v-card-text>
       <v-card-text>
         Please type <strong> Delete </strong> to confirm deletion:
         <v-form>
           <v-text-field
             v-model="deleteDatastreamInput"
-            solo
             @keydown.enter.prevent="onDeleteDatastream"
-          ></v-text-field>
+          />
         </v-form>
       </v-card-text>
       <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn @click="isDeleteModalOpen = false">Cancel</v-btn>
+        <v-spacer />
+        <v-btn @click="closeDeleteModal">Cancel</v-btn>
         <v-btn color="delete" @click="onDeleteDatastream">Confirm</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 
-  <v-dialog v-model="linkDataSourceDialogOpen" persistent>
-    <SiteLinkDataSourceForm
-      @close-dialog="linkDataSourceDialogOpen = false"
-      :thingId="thingId"
-      :datastreamId="linkFormDatastreamId"
-      :dataSourceId="linkFormDataSourceId"
-      :column="linkFormColumn"
+  <v-dialog v-model="openLinker" width="40rem">
+    <DatastreamSourceLinker
+      :datastream="datastream"
+      @close="openLinker = false"
+      @updated="handleLinkUpdated"
     />
   </v-dialog>
 </template>
 
 <script setup lang="ts">
 import { Datastream } from '@/types'
-import { Ref, ref, watch } from 'vue'
-import SiteLinkDataSourceForm from '@/components/Site/SiteLinkDataSourceForm.vue'
+import { ref } from 'vue'
 import { api } from '@/services/api'
 import Notification from '@/utils/notifications'
+import DatastreamSourceLinker from './DatastreamSourceLinker.vue'
 
 const props = defineProps({
   datastream: {
@@ -159,46 +148,44 @@ const props = defineProps({
   },
 })
 
-const emit = defineEmits(['openPlot', 'deleted'])
+const emit = defineEmits(['openPlot', 'deleted', 'linkUpdated'])
+const handleLinkUpdated = (patchBody: {}) => emit('linkUpdated', patchBody)
 
-const selectedDatastream: Ref<Datastream | null> = ref(null)
-const isDeleteModalOpen = ref(false)
+const openDelete = ref(false)
+const openLinker = ref(false)
 const deleteDatastreamInput = ref('')
 
 function closeDeleteModal() {
-  selectedDatastream.value = null
+  openDelete.value = false
   deleteDatastreamInput.value = ''
 }
-
-watch(isDeleteModalOpen, (newValue) => {
-  if (newValue === false) {
-    closeDeleteModal()
-  }
-})
 
 async function toggleDataVisibility(datastream: Datastream) {
   datastream.isDataVisible = !datastream.isDataVisible
   if (datastream.isDataVisible) datastream.isVisible = true
-  onUpdate(datastream)
+  onUpdate({
+    id: datastream.id,
+    isVisible: datastream.isVisible,
+    isDataVisible: datastream.isDataVisible,
+  })
 }
 
 async function toggleVisibility(datastream: Datastream) {
   datastream.isVisible = !datastream.isVisible
   if (!datastream.isVisible) datastream.isDataVisible = false
-  onUpdate(datastream)
+  onUpdate({
+    id: datastream.id,
+    isVisible: datastream.isVisible,
+    isDataVisible: datastream.isDataVisible,
+  })
 }
 
-const onUpdate = async (datastream: Datastream) => {
+const onUpdate = async (patchBody: {}) => {
   try {
-    await api.updateDatastream(datastream)
+    await api.updateDatastream(patchBody as Datastream)
   } catch (error) {
     console.error('Error updating datastream', error)
   }
-}
-
-function openDeleteModal(datastream: Datastream) {
-  selectedDatastream.value = datastream
-  isDeleteModalOpen.value = true
 }
 
 const downloadDatastreamCSV = async (id: string) => {
@@ -225,32 +212,13 @@ async function onDeleteDatastream() {
     return
   }
 
-  isDeleteModalOpen.value = false
-  deleteDatastreamInput.value = ''
+  closeDeleteModal()
 
-  if (selectedDatastream.value) {
-    try {
-      await api.deleteDatastream(selectedDatastream.value.id)
-      emit('deleted')
-    } catch (error) {
-      console.error(`Error deleting datastream`, error)
-    }
+  try {
+    await api.deleteDatastream(props.datastream.id)
+    emit('deleted')
+  } catch (error) {
+    console.error(`Error deleting datastream`, error)
   }
-}
-
-const linkFormDatastreamId = ref()
-const linkFormDataSourceId = ref()
-const linkFormColumn = ref()
-const linkDataSourceDialogOpen = ref(false)
-
-function handleLinkDataSource(
-  datastreamId: string,
-  dataSourceId: string,
-  column: string | number
-) {
-  linkFormDatastreamId.value = datastreamId
-  linkFormDataSourceId.value = dataSourceId
-  linkFormColumn.value = column
-  linkDataSourceDialogOpen.value = true
 }
 </script>
