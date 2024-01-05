b16adea2f2df3280afc96257773b78d45a95fd12
daniel-slaugh <daniel.slaugh@hotmail.com>
2024-01-05 09:54:22 -0700

    Add config directory; add terms of use config

---
+++ src/config/policyConfig.ts
@@ -0,0 +1,6 @@
+export default {
+  termsOfUse:
+    'https://github.com/hydroserver2/hydroserver/blob/main/terms-of-use.md',
+  privacyPolicy:
+    'https://github.com/hydroserver2/hydroserver/blob/main/privacy-policy.md',
+}
