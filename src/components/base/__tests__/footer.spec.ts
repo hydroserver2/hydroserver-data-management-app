00a4ac65701e6a3380cc4863ce71030c21e7301d
daniel-slaugh <daniel.slaugh@hotmail.com>
2024-01-08 08:47:47 -0700

    Add testUtils.ts

--- src/components/base/__tests__/footer.spec.ts
+++ src/components/base/__tests__/footer.spec.ts
@@ -1,33 +1,10 @@
 import { describe, it, expect } from 'vitest'
-import router from '@/router/router'
 import { mount } from '@vue/test-utils'
 import Footer from '@/components/base/Footer.vue'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const testVuetify = createVuetify({
-  components,
-  directives,
-})
-
-global.ResizeObserver = require('resize-observer-polyfill')
-
-const TestComponent = {
-  components: { Footer },
-  template: `
-  <v-app>
-    <v-main>
-      <Footer />
-    </v-main>
-  </v-app>
-  `,
-}
+import { wrapTestComponent, renderOptions } from '@/utils/test/testUtils'
 
 describe('load footer', () => {
-  const wrapper = mount(TestComponent, {
-    global: { plugins: [testVuetify, router] },
-  })
+  const wrapper = mount(wrapTestComponent(Footer), renderOptions())
 
   it('should load', async () => {
     expect(wrapper.text()).toContain('Contact Us')
