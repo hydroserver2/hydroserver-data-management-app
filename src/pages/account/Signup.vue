d4d920100a9bd298edf11c5382c02b26de32d4b2
daniel-slaugh <daniel.slaugh@hotmail.com>
2024-02-21 13:06:54 -0700

    Signup page add sm and lg media queries

--- src/pages/account/Signup.vue
+++ src/pages/account/Signup.vue
@@ -1,18 +1,21 @@
 <template>
   <v-container>
-    <div width="40rem">
-      <AccountForm :has-cancel-button="false">
-        <template v-slot:header>
-          <v-card-title align="center" class="mb-2 signup-title">
-            Sign Up
-          </v-card-title>
-          <v-card-text align="center" class="my-2">
-            Note that all provided information besides your password will be
-            publicly viewable. This helps with communication and data integrity.
-          </v-card-text>
-        </template>
-      </AccountForm>
-    </div>
+    <v-row justify="center" align="center" class="mt-10">
+      <v-col cols="12" sm="8" lg="6">
+        <AccountForm :has-cancel-button="false">
+          <template v-slot:header>
+            <v-card-title align="center" class="mb-2 signup-title">
+              Sign Up
+            </v-card-title>
+            <v-card-text align="center" class="my-2">
+              Note that all provided information besides your password will be
+              publicly viewable. This helps with communication and data
+              integrity.
+            </v-card-text>
+          </template>
+        </AccountForm>
+      </v-col>
+    </v-row>
 
     <v-row justify="center" class="mt-6">
       <span class="mr-2">Already have an account?</span>
