0e1205e1a2765dbb424106942e34a65a84217499
daniel-slaugh <daniel.slaugh@hotmail.com>
2024-04-02 12:33:39 -0600

    Port sparkline from ObservablePlot to Echarts

--- src/components/Sparkline.vue
+++ src/components/Sparkline.vue
@@ -1,17 +1,38 @@
 <template>
   <v-progress-linear v-if="loading" color="secondary" indeterminate />
-  <div @click="handleEmit" ref="chart"></div>
+  <div v-else-if="!loading && obs72.length" @click="handleEmit">
+    <v-chart
+      :option="chartOption"
+      autoresize
+      style="height: 100px; width: 300px"
+    />
+  </div>
+  <div v-else-if="!obs72.length">No data for this datastream</div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import * as Plot from '@observablehq/plot'
-import * as d3 from 'd3'
+import { ref, onMounted, computed } from 'vue'
 import { PropType } from 'vue'
+import VChart from 'vue-echarts'
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { LineChart } from 'echarts/charts'
 import { DataArray, Datastream } from '@/types'
-import { preProcessData } from '@/utils/observationsUtils'
-import { subtractHours } from '@/utils/observationsUtils'
+import { preProcessData, subtractHours } from '@/utils/observationsUtils'
 import { useObservationStore } from '@/store/observations'
+import {
+  GridComponent,
+  TooltipComponent,
+  DataZoomComponent,
+} from 'echarts/components'
+
+use([
+  CanvasRenderer,
+  LineChart,
+  GridComponent,
+  TooltipComponent,
+  DataZoomComponent,
+])
 
 const { fetchObservationsInRange } = useObservationStore()
 
@@ -21,60 +42,64 @@ const props = defineProps({
     required: true,
   },
 })
+
 const emit = defineEmits(['openChart'])
 const handleEmit = () => {
+  console.log('handling emit')
   emit('openChart')
 }
 
 const obs72 = ref<DataArray>([])
 const loading = ref(true)
-const chart = ref<HTMLDivElement | null>(null)
+
+const chartOption = computed(() => {
+  const observations = preProcessData(obs72.value, props.datastream)
+  if (!observations.length) return {}
+
+  let colors = isStale(props.datastream.phenomenonEndTime)
+    ? { line: '#9E9E9E', fill: '#EEEEEE', border: '#BDBDBD' } // Grey, GL4, GL1
+    : { line: '#4CAF50', fill: '#E8F5E9', border: '#BDBDBD' } // Green, GrL5, GL1
+
+  return {
+    color: [colors.line],
+    grid: {
+      bottom: 3,
+      right: 1,
+      top: 3,
+      left: 1,
+      borderColor: colors.border,
+      borderWidth: 2,
+      show: true,
+    },
+    xAxis: {
+      type: 'time',
+      show: false,
+    },
+    yAxis: {
+      type: 'value',
+      min: 'dataMin',
+      max: 'dataMax',
+      show: false,
+    },
+    series: [
+      {
+        type: 'line',
+        data: observations.map((dp) => [dp.date.getTime(), dp.value]),
+        showSymbol: false,
+        areaStyle: {
+          color: colors.fill,
+          origin: 'start',
+        },
+      },
+    ],
+  }
+})
 
 function isStale(timestamp: string | null | undefined) {
   if (!timestamp) return true
   let endTime = new Date(timestamp)
   let seventyTwoHoursAgo = new Date(Date.now() - 72 * 60 * 60 * 1000)
   return endTime < seventyTwoHoursAgo
-}
-
-function drawChart() {
-  if (!chart.value) return
-
-  let colors = isStale(props.datastream.phenomenonEndTime)
-    ? { line: '#9E9E9E', fill: '#F5F5F5' } // Grey and grey-lighten-4
-    : { line: '#4CAF50', fill: '#E8F5E9' } // Green and green-lighten-5
-
-  let observations = preProcessData(obs72.value, props.datastream)
-  const [minY, maxY] = d3.extent(observations, (d) => d.value)
-
-  const sparkline = Plot.plot({
-    width: 250,
-    height: 100,
-    x: {
-      grid: false,
-      axis: null,
-    },
-    y: {
-      domain: [minY, maxY],
-      grid: false,
-      axis: null,
-    },
-    marks: [
-      Plot.areaY(observations, {
-        x: 'date',
-        y1: (d) => minY,
-        y2: 'value',
-        stroke: colors.line,
-        fill: colors.fill,
-      }),
-      Plot.frame({
-        stroke: 'grey',
-      }),
-    ],
-  })
-
-  chart.value.innerHTML = ''
-  chart.value.appendChild(sparkline)
 }
 
 const fetchObsLast72Hours = async (ds: Datastream) => {
@@ -89,13 +114,7 @@ const fetchObsLast72Hours = async (ds: Datastream) => {
 }
 
 onMounted(async () => {
-  obs72.value = await fetchObsLast72Hours(props.datastream)
+  obs72.value = (await fetchObsLast72Hours(props.datastream)) || []
   loading.value = false
-
-  if (!obs72.value?.length && chart.value) {
-    chart.value.textContent = 'No data for this datastream'
-  } else {
-    drawChart()
-  }
 })
 </script>
