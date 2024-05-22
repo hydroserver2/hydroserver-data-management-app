85efa0dbbf6a0954fea067b4c592ec3680c3f526
Kenneth Lippold <kjlippold@outlook.com>
2024-05-22 09:35:47 -0700

    Update policyConfig.ts

--- src/config/policyConfig.ts
+++ src/config/policyConfig.ts
@@ -1,6 +1,6 @@
 export default {
   termsOfUse:
-    'https://github.com/hydroserver2/hydroserver-webapp-front/blob/main/src/assets/terms-of-use-example.md',
+    'https://github.com/hydroserver2/hydroserver-data-management-app/blob/main/src/assets/terms-of-use-example.md',
   privacyPolicy:
-    'https://github.com/hydroserver2/hydroserver-webapp-front/blob/main/src/assets/privacy-policy-example.md',
+    'https://github.com/hydroserver2/hydroserver-data-management-app/blob/main/src/assets/privacy-policy-example.md',
 }
