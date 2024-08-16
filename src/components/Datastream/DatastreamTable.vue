0cadd31776da6f3c369c23cc3e9f74d913b20fcf
daniel-slaugh <daniel.slaugh@hotmail.com>
2024-08-16 11:49:51 -0600

    Make DatastreamTable virtual

--- src/components/Datastream/DatastreamTable.vue
+++ src/components/Datastream/DatastreamTable.vue
@@ -25,11 +25,13 @@
     {{ thing?.dataDisclaimer }}
   </h6>
 
-  <v-data-table
+  <v-data-table-virtual
     class="elevation-3 my-4"
     :headers="headers"
     :items="visibleDatastreams"
     v-model:sort-by="sortBy"
+    :style="{ 'max-height': `100vh` }"
+    fixed-header
   >
     <template v-slot:item.info="{ item }">
       <v-col>
@@ -98,7 +100,7 @@
         @linkUpdated="loadDatastreams"
       />
     </template>
-  </v-data-table>
+  </v-data-table-virtual>
 </template>
 
 <script setup lang="ts">
