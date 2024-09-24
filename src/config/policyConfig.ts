28796889e2fbf27b7902e1f8be8f325fa67b23a8
daniel-slaugh <daniel.slaugh@hotmail.com>
2024-09-24 10:44:19 -0600

    Simplify footer; Allow configurations from footerConfig.ts

--- src/config/policyConfig.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  termsOfUse:
-    'https://github.com/hydroserver2/hydroserver-data-management-app/blob/main/src/assets/terms-of-use-example.md',
-  privacyPolicy:
-    'https://github.com/hydroserver2/hydroserver-data-management-app/blob/main/src/assets/privacy-policy-example.md',
-}
