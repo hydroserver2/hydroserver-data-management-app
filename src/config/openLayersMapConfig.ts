14aecdd329425948633518e595c82cf95b5b743d
Daniel Slaugh <daniel.slaugh@hotmail.com>
2025-06-03 13:07:50 -0600

    Clean up comments

--- src/config/openLayersMapConfig.ts
+++ src/config/openLayersMapConfig.ts
@@ -4,15 +4,8 @@ const defaultLatitude = 39
 const defaultLongitude = -100
 
 export const defaultOpenLayersMapOptions = {
-  /**
-   * Center of the map in [longitude, latitude] (EPSG:4326).
-   * Will be converted to EPSG:3857 automatically in the map component.
-   */
-  // center: [-100, 39],
   center: fromLonLat([defaultLongitude, defaultLatitude]),
   zoom: 4,
-  //   minZoom: 2,
-  //   maxZoom: 18,
   autoFitToMarkers: true,
   tileSource: {
     type: 'OSM',
