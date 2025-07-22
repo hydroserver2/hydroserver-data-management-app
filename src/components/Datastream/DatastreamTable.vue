64042e83d6cb5692416f2596cfdec0a5c4751916
Daniel Slaugh <daniel.slaugh@hotmail.com>
2025-07-22 12:47:25 -0600

    Update sparklines to show 30:50:200 data points based on intended time spacing

--- src/components/Datastream/DatastreamTable.vue
+++ src/components/Datastream/DatastreamTable.vue
@@ -478,7 +478,7 @@ const headers = [
     sortable: false,
   },
   {
-    title: 'Observations (Last 100 values)',
+    title: 'Observation information',
     key: 'observations',
     sortable: false,
   },
