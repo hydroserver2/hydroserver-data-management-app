e823d34c0ac174a2a266573fed2ca0ab5829ee9a
Ken Lippold <klippold@Kens-MacBook-Pro-2.local>
2025-07-29 08:07:17 -0700

    Update release workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,80 +5,85 @@ import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({
-      autoImport: true,
-      styles: { configFile: 'src/styles/settings.scss' },
-    }),
-  ],
-  optimizeDeps: {
-    exclude: ['vuetify'],
-  },
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-  },
-  resolve: {
-    extensions: ['.js', '.json', '.vue', '.less', '.scss', '.ts'],
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(({ mode, command }) => {
+  const isProduction = command === 'build'
+
+  return {
+    base: isProduction ? '/static/web/' : '/',
+    plugins: [
+      vue(),
+      vuetify({
+        autoImport: true,
+        styles: { configFile: 'src/styles/settings.scss' },
+      }),
+    ],
+    optimizeDeps: {
+      exclude: ['vuetify'],
     },
-  },
-  build: {
-    manifest: true,
-  },
-  test: {
-    globals: true,
-    environmentMatchGlobs: [['src/components/**', 'jsdom']],
     server: {
-      deps: {
-        inline: ['vuetify'],
+      host: '127.0.0.1',
+      port: 5173,
+    },
+    resolve: {
+      extensions: ['.js', '.json', '.vue', '.less', '.scss', '.ts'],
+      alias: {
+        '@': resolve(__dirname, 'src'),
       },
     },
-    environment: 'jsdom',
-    coverage: {
-      include: ['src/**/*.{ts,tsx,js,jsx}'],
-      exclude: [
-        '**/src/composables/useWorkspaceTags.ts',
-        '**/src/composables/useHydroShare.ts',
-        '**/src/composables/useWorkspacePermissions.ts',
-        '**/src/composables/useMetadata.ts',
-        '**/src/composables/useVocabulary.ts',
-        '**/src/services/getCSRFToken.ts',
-        '**/src/models/**',
-        '**/src/plugins/**',
-        '**/src/router/**',
-        '**/src/store/**',
-        '**/src/types/**',
-        '**/src/config/**',
-        '**/src/services/api.ts',
-        '**/src/services/apiMethods.ts',
-        '**/src/services/handle401.ts',
-        '**/src/utils/mdi-icons.ts',
-        '**/src/utils/materialColors.ts',
-        '**/src/utils/CSVDownloadUtils.ts',
-        '**/src/utils/plotting/echarts.ts',
-        '**/src/utils/plotting/graphSeriesUtils.ts',
-        '**/src/utils/test/**',
-        '**/src/utils/time.ts',
-        '**/src/utils/googleMaps/**',
-        '**/src/utils/maps/**',
-        '**/src/utils/rules.ts',
-        '**/src/App.vue',
-        '**/src/main.ts',
-        '**/*.d.ts',
-        '**/postcss.config.js',
-      ],
-      thresholds: {
-        lines: 80,
-        statements: 80,
-        functions: 80,
-        branches: 80,
-      },
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+    build: {
+      manifest: true,
     },
-  },
+    test: {
+      globals: true,
+      environmentMatchGlobs: [['src/components/**', 'jsdom']],
+      server: {
+        deps: {
+          inline: ['vuetify'],
+        },
+      },
+      environment: 'jsdom',
+      coverage: {
+        include: ['src/**/*.{ts,tsx,js,jsx}'],
+        exclude: [
+          '**/src/composables/useWorkspaceTags.ts',
+          '**/src/composables/useHydroShare.ts',
+          '**/src/composables/useWorkspacePermissions.ts',
+          '**/src/composables/useMetadata.ts',
+          '**/src/composables/useVocabulary.ts',
+          '**/src/services/getCSRFToken.ts',
+          '**/src/models/**',
+          '**/src/plugins/**',
+          '**/src/router/**',
+          '**/src/store/**',
+          '**/src/types/**',
+          '**/src/config/**',
+          '**/src/services/api.ts',
+          '**/src/services/apiMethods.ts',
+          '**/src/services/handle401.ts',
+          '**/src/utils/mdi-icons.ts',
+          '**/src/utils/materialColors.ts',
+          '**/src/utils/CSVDownloadUtils.ts',
+          '**/src/utils/plotting/echarts.ts',
+          '**/src/utils/plotting/graphSeriesUtils.ts',
+          '**/src/utils/test/**',
+          '**/src/utils/time.ts',
+          '**/src/utils/googleMaps/**',
+          '**/src/utils/maps/**',
+          '**/src/utils/rules.ts',
+          '**/src/App.vue',
+          '**/src/main.ts',
+          '**/*.d.ts',
+          '**/postcss.config.js',
+        ],
+        thresholds: {
+          lines: 80,
+          statements: 80,
+          functions: 80,
+          branches: 80,
+        },
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+      },
+    },
+  }
 })
