8373cb70231e747e814eebfdc3b63e01dedd5d32
Daniel Slaugh <daniel.slaugh@hotmail.com>
2025-08-07 14:03:46 -0600

    Fix datasourceDetails and metadata pages not loading sometimes

--- src/store/metadata.ts
+++ src/store/metadata.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useMetadata = defineStore('etl', () => {
+export const useMetadata = defineStore('metadata', () => {
   const tab = ref(0)
 
   return {
