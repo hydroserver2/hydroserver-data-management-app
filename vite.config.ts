a46dac0c0a98e21502f7c1936b6928fc3f677496
Daniel Slaugh <daniel.slaugh@hotmail.com>
2026-01-23 13:58:35 -0700

    Add Tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig(({ mode }) => {
@@ -17,6 +18,7 @@ export default defineConfig(({ mode }) => {
     base: mode === 'django' ? '/static/web/' : '',
     plugins: [
       vue(),
+      tailwindcss(),
       vuetify({
         autoImport: true,
         styles: { configFile: 'src/styles/settings.scss' },
